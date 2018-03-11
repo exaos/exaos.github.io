648449c516ef5c755136b824f2c626dcb10298a1
Vivodo Lio <vivodo@outlook.com>
2018-03-11 22:16:02 +0800

    Site re-generated with updated configurations and css files.

--- assets/js/pagetail.js
+++ assets/js/pagetail.js
@@ -1,14 +1,18 @@
-
 /* disqus code */
 var disqus_shortname = 'exaos-github';
-(function() { var dsq = document.createElement('script'); dsq.type =
-'text/javascript'; dsq.async = true; dsq.src = '//' + disqus_shortname +
-'.disqus.com/embed.js';  (document.getElementsByTagName('head')[0] || document.getElementsByTagName('body')[0]).appendChild(dsq); })();
+(function() {
+    var dsq = document.createElement('script');
+    dsq.type = 'text/javascript';
+    dsq.async = true; dsq.src = '//' +
+        disqus_shortname + '.disqus.com/embed.js';
+    (document.getElementsByTagName('head')[0] ||
+     document.getElementsByTagName('body')[0]).appendChild(dsq); })();
 
 /* Google Analytics */
-(function(i,s,o,g,r,a,m){i['GoogleAnalyticsObject']=r;i[r]=i[r]||function(){
-(i[r].q=i[r].q||[]).push(arguments)},i[r].l=1*new Date();a=s.createElement(o),
-m=s.getElementsByTagName(o)[0];a.async=1;a.src=g;m.parentNode.insertBefore(a,m)
+(function(i,s,o,g,r,a,m){
+    i['GoogleAnalyticsObject']=r;i[r]=i[r]||function(){
+        (i[r].q=i[r].q||[]).push(arguments)},i[r].l=1*new Date();a=s.createElement(o),
+    m=s.getElementsByTagName(o)[0];a.async=1;a.src=g;m.parentNode.insertBefore(a,m)
 })(window,document,'script','//www.google-analytics.com/analytics.js','ga');
 
 ga('create', 'UA-45051749-1', 'exaos.github.io');
